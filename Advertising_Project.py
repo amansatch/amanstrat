baa86a21eb7df7a3c4a963f902bc6b06fe6119b5
amansatch <126454511+amansatch@users.noreply.github.com>
2023-03-07 14:32:21 +0800

    Update Advertising_Project.py

--- Advertising_Project.py
+++ Advertising_Project.py
@@ -13,5 +13,20 @@ st.sidebar.header('User Input Parameters')
 x = df['TV']
 y = df['Sales']
 
+w0 = 7
+w1 = 7
+
+for i in range(0,50):
+    yh = w0 + w1*x
+
+    dew0 = -2 * ((y-yh)).mean()
+    dew1 = -2 * ((y-yh) * x).mean()
+
+    lr = 0.00001
+    w0 = w0 - lr*dew0
+    w1 = w1 - lr*dew1
+
+    error = ((y-yh)**2).mean()
+    print(i,error)
 
 
