bd3dadaec43f852af5565590ae52dc6e2bba8056
amansatch <126454511+amansatch@users.noreply.github.com>
2023-03-07 14:47:43 +0800

    Update Advertising_Project.py

--- Advertising_Project.py
+++ Advertising_Project.py
@@ -2,7 +2,12 @@ import streamlit as st
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
-df = pd.read_csv('Advertising.csv')
+
+# Data Visualisation
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+advertising = pd.read_csv('Advertising.csv')
 
 st.write("""
 # Simple Advertising Prediction App
@@ -11,8 +16,14 @@ This app predicts the **Advertising** type!
 
 st.sidebar.header('User Input Parameters')
 
-df = df.drop('Unnamed: 0', axis=1)
-fig = px.histogram(df, x="sales")
-fig.show()
+# Checking Null values
+advertising.isnull().sum()*100/advertising.shape[0]
+# There are no NULL values in the dataset, hence it is clean.
 
+# Outlier Analysis
+fig, axs = plt.subplots(3, figsize = (5,5))
+plt1 = sns.boxplot(advertising['TV'], ax = axs[0])
+plt2 = sns.boxplot(advertising['Newspaper'], ax = axs[1])
+plt3 = sns.boxplot(advertising['Radio'], ax = axs[2])
+plt.tight_layout()
 
