87f01e243d88e490a12705d0e2d8518c99959103
amansatch <126454511+amansatch@users.noreply.github.com>
2023-03-07 14:52:48 +0800

    Update Advertising_Project.py

--- Advertising_Project.py
+++ Advertising_Project.py
@@ -16,14 +16,10 @@ This app predicts the **Advertising** type!
 
 st.sidebar.header('User Input Parameters')
 
-# Checking Null values
-advertising.isnull().sum()*100/advertising.shape[0]
-# There are no NULL values in the dataset, hence it is clean.
-
 # Outlier Analysis
 fig, axs = plt.subplots(3, figsize = (5,5))
 plt1 = sns.boxplot(advertising['TV'], ax = axs[0])
 plt2 = sns.boxplot(advertising['Newspaper'], ax = axs[1])
 plt3 = sns.boxplot(advertising['Radio'], ax = axs[2])
-plt.tight_layout()
+st.plt.tight_layout()
 
