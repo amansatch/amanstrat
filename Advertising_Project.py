0cd371a1f4fa6d6d7638229f17729b7ab9e64fc6
amansatch <126454511+amansatch@users.noreply.github.com>
2023-03-07 15:59:50 +0800

    Update Advertising_Project.py

--- Advertising_Project.py
+++ Advertising_Project.py
@@ -14,14 +14,13 @@ This app predicts the **Advertising** type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    sepal_length = st.sidebar.slider('TV length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
+    adsTV = st.sidebar.slider('TV length', 4.3, 7.9, 5.4)
+    adsRadio = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    adsNewspaper = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+        data = {'sepal_length': sepal_length,
             'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
+            'petal_length': petal_length
+            }
     features = pd.DataFrame(data, index=[0])
     return features
 
